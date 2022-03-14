68a6da7988a23cfdb04a9b9bd2f0f1ed12fe021e
ericbertrand8686 <ericbertrand8686@yahoo.fr>
2022-03-14 14:05:48 +0100

    updt : muzeo 2 voiture

---
+++ brief_voiture_scraping/brief_voiture_scraping/spiders/voiture_spider.py
@@ -0,0 +1,35 @@
+import string
+import scrapy
+import re
+
+class muzeoSpider(scrapy.Spider):
+
+    name = "voiture"
+    
+    def start_requests(self):
+
+        res = []
+        with open("liste_url.txt") as f :
+             urls = [url.strip()[1:-1] for url in f.readlines()]
+        
+        urls=urls[0:10]
+
+        for url in urls:
+            res.append(scrapy.Request(url=url, callback=self.parse))
+
+        return res
+
+
+    def parse(self, response):
+        yield {
+                "Version":  response.xpath('//*[@id="mes-ht"]/ul/li[1]/span/text()').extract_first(),
+                "Prix": response.xpath('//*[@id="mes-ht"]/ul/li[2]/span/text()').extract_first(),
+                "Année":  response.xpath( '//*[@id="mes-ht"]/ul/li[3]/span/text()').extract_first(),
+                "Kilométrage": response.xpath( '//*[@id="mes-ht"]/ul/li[4]/span/text()').extract_first(),
+                "Energie": response.xpath( '//*[@id="mes-ht"]/ul/li[5]/span/text()').extract_first(),
+                "Emissions": response.xpath( '//*[@id="mes-ht"]/ul/li[6]/span/text()').extract_first(),
+                "Consommation": response.xpath( '//*[@id="mes-ht"]/ul/li[7]/span/text()').extract_first(),
+                "Transmission": response.xpath( '//div[@id="papier_peint_mes_img"]').extract_first(),
+                "Portes":  response.xpath( '//*[@id="mes-ht"]/ul/li[9]/span/text()').extract_first()
+        }
+        return
